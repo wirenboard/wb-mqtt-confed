5868e40361fc2e588ebc03bfc35949c7f776b012
Ivan Shvedunov <ivan4th@gmail.com>
2015-09-02 13:58:45 +0300

    Don't forget to close files after reading.

--- confed/util.go
+++ confed/util.go
@@ -11,6 +11,7 @@ func loadConfigBytes(path string) (bs []byte, err error) {
 	if err != nil {
 		return
 	}
+	defer in.Close() // not writing the file, so we can ignore Close() errors here
 	reader := JsonConfigReader.New(in)
 	return ioutil.ReadAll(reader)
 }
